dc2801468cfe79ffb0e5641b74d5860e29a9acd0
Uzma Shaikh <uzma1109@yahoo.com>
2021-11-18 03:05:02 +0400

    Adding lessons array

---
+++ lessons.js
@@ -0,0 +1,110 @@
+let lessons = [
+  {
+    "id": 1,
+    "subject": "Dance",
+    "location": "London",
+    "price": 7500,
+    "image": "dance.png",
+    "availableInventory": 5,
+  
+  },
+  {
+      "id": 2,
+      "subject": "Cooking",
+      "location": "Bristol",
+      "price": 5000,
+      "image": "cooking.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 3,
+      "subject": "Art",
+      "location": "England",
+      "price": 1300,
+      "image": "art.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 4,
+      "subject": "Swimming",
+      "location": "Berlin",
+      "price": 7000,
+      "image": "swimming.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 5,
+      "subject": "Robotics",
+      "location": "Tokyo",
+      "price": 3000,
+      "image": "robotics.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 6,
+      "subject": "Chess",
+      "location": "Moscow",
+      "price": 1000,
+      "image": "chess.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 7,
+      "subject": "Yoga",
+      "location": "Rio",
+      "price": 6000,
+      "image": "yoga.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 8,
+      "subject": "Baseball",
+      "location": "India",
+      "price": 2000,
+      "image": "baseball.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 9,
+      "subject": "Basketball",
+      "location": "Ireland",
+      "price": 3000,
+      "image": "basketball.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 10,
+      "subject": "Badminton",
+      "location": "Denver",
+      "price": 4000,
+      "image": "badminton.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 11,
+      "subject": "Music",
+      "location": "Atlanta",
+      "price": 2050,
+      "image": "music.png",
+      "availableInventory": 5,
+    
+    },
+    {
+      "id": 12,
+      "subject": "Gymnastic",
+      "location": "Dubai",
+      "price": 3050,
+      "image": "gymnastic.png",
+      "availableInventory": 5,
+    }
+]
+ 
